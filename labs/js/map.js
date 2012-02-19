236bbcbd5d41c4b2bef6348af203635a99d59f1b
Elvin Priyadi <elvinpriyadi@gmail.com>
2012-02-19 14:37:30 +0100

    issues 5-6

--- labs/js/map.js
+++ labs/js/map.js
@@ -2,12 +2,11 @@
 var rootURL = "http://localhost/labs/geoip.php/";
 var currentLocation;
 
-console.log("root");
+// GetDefaultIp onLoad event
 $(document).ready(getDefaultIp);
 
 // Register listeners onClick
 $('#btnSearch').click(function() {
-console.info("btnsearch clicked");
 	findByIp($('#ipaddress').val());
 	return false;
 });
@@ -21,7 +20,6 @@ $('#ipaddress').keypress(function(e){
 
 // AJAX call to WebService
 function findByIp(ip) {
-	console.log('findById: ' + ip);
 	$.ajax({
 		type: 'GET',
 		url: rootURL + 'ip/' + ip,
@@ -30,12 +28,9 @@ function findByIp(ip) {
 			console.log("success", data);
 			var IP = $(data).find('IP').text()
 			var countryName = $(data).find('CountryName').text();
-			console.log("IP", IP);
-			console.log("CountryName", countryName);
 			convertToGeoCode(countryName, ip);
 			
 			toggleError();
-			
 		},
 		error: function(data){
 			var error = $(data.responseText).find('ReturnCodeDetails').text();
@@ -46,7 +41,6 @@ function findByIp(ip) {
 
 // Fill IP address by default
 function getDefaultIp() {
-	console.log('getDefaultIp: ');
 	$.ajax({
 		type: 'GET',
 		url: rootURL + 'ip/default',
@@ -61,19 +55,16 @@ function getDefaultIp() {
 
 // Convert Country name to Geocode
 function convertToGeoCode(countryName, ip) {
-var geocoder = new google.maps.Geocoder();
-geocoder.geocode({ 'address': countryName}, function(results, status) {
-console.log("status", google.maps.GeocoderStatus.OK);
-  if (status == google.maps.GeocoderStatus.OK) {
-  console.log(results[0].geometry.location.lat);
-	initializeMap(results[0].geometry.location, countryName, ip);
-	};
-  })
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({ 'address': countryName}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			initializeMap(results[0].geometry.location, countryName, ip);
+		};
+	})
 }
 
 // Initialize map object
 function initializeMap(address, countryName, ip) {
-console.log("address", address);
   var myOptions = {
 	zoom: 6,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -95,9 +86,9 @@ console.log("address", address);
 // Show or Hide error
 function toggleError(error) {
 	if(error != null){
-		$('#error').show();
+		$('#error').show().addClass("error");
 		$('#error').text(error);		
 	} else {
-		$('#error').hide();
+		$('#error').removeClass("error").hide();
 	}
 }
